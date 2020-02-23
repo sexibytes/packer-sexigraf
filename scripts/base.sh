a11c7134f4e177eeaa90b9152d14a3b054d2e15f
Raphaël SCHITZ <raphael@schitz.net>
2020-02-23 21:15:22 +0000

    htop

--- scripts/base.sh
+++ scripts/base.sh
@@ -2,7 +2,7 @@
 DEBIAN_FRONTEND=noninteractive apt-get -y update
 # apt-get -y install linux-headers-$(uname -r) build-essential
 # apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
-DEBIAN_FRONTEND=noninteractive apt-get -y install curl unzip resolvconf console-setup apt-transport-https vim wget
+DEBIAN_FRONTEND=noninteractive apt-get -y install curl unzip resolvconf console-setup apt-transport-https vim wget htop
 
 # Tweak sshd to prevent DNS resolution (speed up logins)
 echo 'UseDNS no' >> /etc/ssh/sshd_config
