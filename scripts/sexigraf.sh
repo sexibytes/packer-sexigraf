e01488c51d6756ae3d619d46cdb634642a769ea0
Hypervisor <raphael@schitz.net>
2020-02-24 15:45:41 +0100

    cpanm up to date

--- scripts/sexigraf.sh
+++ scripts/sexigraf.sh
@@ -5,30 +5,38 @@ mkdir /var/log/sexigraf/
 # echo "deb http://http.us.debian.org/debian sid main contrib non-free" >> /etc/apt/sources.list
 apt-get update -y
 # DEBIAN_FRONTEND=noninteractive apt-get -y -t unstable --no-install-recommends install graphite-carbon graphite-web collectd
-DEBIAN_FRONTEND=noninteractive apt-get -y --allow-downgrades --allow-remove-essential --allow-change-held-packages --no-install-recommends install libhtml-template-perl libnumber-bytes-human-perl xml-twig-tools libsnmp30 cpanminus genisoimage collectd
+DEBIAN_FRONTEND=noninteractive apt-get -y install libhtml-template-perl libnumber-bytes-human-perl xml-twig-tools libsnmp30 cpanminus genisoimage collectd
 
 /bin/cp -rf /tmp/sexigraf-dev6/root/* /root/
 tar -zxf /root/VMware-vSphere-Perl-SDK-6.7.0-8156551.x86_64.tar.gz -C /root/
 
-cpanm ExtUtils::MakeMaker@6.96
-cpanm Net::FTP@2.77
-cpanm UUID::Random@0.04
+# cpanm ExtUtils::MakeMaker@6.96
+# cpanm Net::FTP@2.77
+# cpanm UUID::Random@0.04
 cpanm UUID
-cpanm Net::Graphite@0.16
-cpanm Module::Build@0.4205 --force
-cpanm Text::Template@1.47
+# cpanm Net::Graphite@0.16
+# cpanm Module::Build@0.4205 --force
+# cpanm Text::Template@1.47
 
-cd /root/cpan-packages/
-for i in /root/cpan-packages/*.tar.gz; do tar -xvzf $i; done
+cpanm LWP
+cpanm Text::Template
+cpanm Module::Build
+cpanm Net::Graphite
+cpanm UUID::Random
+cpanm ExtUtils::MakeMaker
 
-# Theses packages are not available through deb files, compilation is required
-cd /root/cpan-packages/libwww-perl-5.837; perl Makefile.PL; make; make install
+# cd /root/cpan-packages/
+# for i in /root/cpan-packages/*.tar.gz; do tar -xvzf $i; done
+
+# # Theses packages are not available through deb files, compilation is required
+# cd /root/cpan-packages/libwww-perl-5.837; perl Makefile.PL; make; make install
 
 # Remove uneccessary packages used for compliation
 # apt-get -y purge gcc-4.9-base:i386
 # dpkg --remove-architecture i386
 
 sed -i 's/ubuntu/debian/g' /root/vmware-vsphere-cli-distrib/vmware-install.pl
+sed -i 's/# $ENV{PERL_NET_HTTPS_SSL_SOCKET_CLASS} = "Net::SSL";/$ENV{PERL_NET_HTTPS_SSL_SOCKET_CLASS} = "Net::SSL";/g' /root/vmware-vsphere-cli-distrib/lib/VMware/share/VMware/VICommon.pm
 yes | PAGER=cat /root/vmware-vsphere-cli-distrib/vmware-install.pl default
 
 /bin/cp -rf /tmp/sexigraf-dev6/etc/* /etc/
@@ -50,7 +58,7 @@ cat >/var/www/.vmware/credstore/vicredentials.xml <<EOL
 EOL
 chown -R www-data. /var/www/
 
-chown chown root:grafana /etc/grafana/provisioning/dashboards/*.yaml
+chown root:grafana /etc/grafana/provisioning/dashboards/*.yaml
 
 # https://github.com/grafana/grafana/issues/15647
 sed -i 's/;disable_sanitize_html = false/disable_sanitize_html = true/g' /etc/grafana/grafana.ini
@@ -72,7 +80,7 @@ echo "\n@reboot         root    /bin/bash /root/PullGuestInfo.sh" >> /etc/cronta
 echo "Removing unused files"
 rm -f /root/VMware-vSphere-Perl-SDK-6.7.0-8156551.x86_64.tar.gz
 rm -rf /root/vmware-vsphere-cli-distrib/
-rm -rf /root/cpan-packages/
-rm -f /root/sexiauditor.patch
-rm -f /root/VICommon.pm
+# rm -rf /root/cpan-packages/
+# rm -f /root/sexiauditor.patch
+# rm -f /root/VICommon.pm
 echo "SexiDone"
