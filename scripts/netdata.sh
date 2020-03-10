64e02d8b74a8effe5500aff5467e6dc66afc7b08
Hypervisor <raphael@schitz.net>
2020-03-10 11:39:14 +0100

    Update netdata.sh

--- scripts/netdata.sh
+++ scripts/netdata.sh
@@ -1,7 +1,9 @@
 # https://github.com/netdata/netdata/tree/master/packaging/installer#automatic-one-line-installation-script
 # DEBIAN_FRONTEND=noninteractive  apt-get -y install autoconf-archive autogen cmake libjudy-dev liblz4-dev libmnl-dev libuv1-dev python3-pymongo
 # 
-bash <(curl -Ss https://my-netdata.io/kickstart.sh) all --dont-wait --non-interactive --no-updates --stable-channel
+wget https://my-netdata.io/kickstart.sh -O /root/kickstart.sh
+bash /root/kickstart.sh all --dont-wait --non-interactive --no-updates --stable-channel
+# bash <(curl -Ss https://my-netdata.io/kickstart.sh) all --dont-wait --non-interactive --no-updates --stable-channel
 # https://github.com/netdata/netdata/issues/1735
 sed -i -e "s/# x-frame-options response header =/x-frame-options response header = sameorigin/g" /etc/netdata/netdata.conf
 sed -i -e "s/# hostname = sexigraf/# hostname = sexigraf\n          bind socket to IP = 127.0.0.1/g" /etc/netdata/netdata.conf
