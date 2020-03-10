e23486e3b99915a3d1e0bcf7715951757782ecd6
Hypervisor <raphael@schitz.net>
2020-03-10 18:28:02 +0100

    Update zfs.sh

--- scripts/zfs.sh
+++ scripts/zfs.sh
@@ -1,5 +1,5 @@
 #https://github.com/openzfs/zfs/wiki/Building-ZFS
-DEBIAN_FRONTEND=noninteractive  apt-get install -y build-essential autoconf automake libtool gawk alien fakeroot dkms libblkid-dev uuid-dev libudev-dev libssl-dev zlib1g-dev libaio-dev libattr1-dev libelf-dev linux-headers-$(uname -r) python3 python3-dev python3-setuptools python3-cffi libffi-dev
+DEBIAN_FRONTEND=noninteractive  apt-get install -y build-essential autoconf automake libtool gawk alien fakeroot dkms libblkid-dev uuid-dev libudev-dev libssl-dev zlib1g-dev libaio-dev libattr1-dev libelf-dev linux-headers-$(uname -r) python3 python3-dev python3-setuptools python3-cffi libffi-dev parted
 # 
 cd /usr/local/src
 git clone https://github.com/openzfs/zfs -b zfs-0.8-release
