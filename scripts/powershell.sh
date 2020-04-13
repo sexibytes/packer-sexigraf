685481824f9dba9be0176911c144c518550d4c2f
Hypervisor <raphael@schitz.net>
2020-04-13 02:06:52 +0200

    Update powershell.sh

--- scripts/powershell.sh
+++ scripts/powershell.sh
@@ -1,6 +1,6 @@
 # https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux?view=powershell-7
 # Download the Microsoft repository GPG keys
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
 
 # Register the Microsoft repository GPG keys
 dpkg -i packages-microsoft-prod.deb
