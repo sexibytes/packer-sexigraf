9e1af46b6fbb5ebac740c14ed80436ecceb48b32
Hypervisor <raphael@schitz.net>
2020-04-16 17:17:36 +0200

    remove zfs support

--- scripts/base.sh
+++ scripts/base.sh
@@ -2,12 +2,30 @@
 DEBIAN_FRONTEND=noninteractive apt-get -y update
 # apt-get -y install linux-headers-$(uname -r) build-essential
 # apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
-DEBIAN_FRONTEND=noninteractive apt-get -y install curl unzip resolvconf console-setup apt-transport-https vim wget htop
+DEBIAN_FRONTEND=noninteractive apt-get -y install curl unzip resolvconf console-setup apt-transport-https vim wget htop parted
 
 # Tweak sshd to prevent DNS resolution (speed up logins)
 # echo 'UseDNS no' >> /etc/ssh/sshd_config
 sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/#PermitRootLogin/PermitRootLogin/' /etc/ssh/sshd_config
 
-# vmwgfx.enable_fbdev=1
 # https://communities.vmware.com/thread/514376
+# vmwgfx.enable_fbdev=1
+
+# https://kb.vmware.com/s/article/2053145
+echo "options vmw_pvscsi cmd_per_lun=254 ring_pages=32" > /etc/modprobe.d/pvscsi
+
+if fdisk -l|grep -i "/dev/sdb" > /dev/null; then
+# https://www.digitalocean.com/community/tutorials/how-to-partition-and-format-storage-devices-in-linux
+# https://askubuntu.com/questions/384062/how-do-i-create-and-tune-an-ext4-partition-from-the-command-line
+  echo "mount sdb"
+  parted /dev/sdb mklabel gpt
+  parted -a opt /dev/sdb mkpart primary ext4 0% 100%
+  mkfs.ext4 -L wfs /dev/sdb1
+  tune2fs -o journal_data_writeback /dev/sdb1
+  mkdir -p /mnt/wfs
+  echo "#" >> /etc/fstab
+  echo "LABEL=wfs /mnt/wfs ext4 noatime,nodiratime,data=writeback,barrier=0,nobh,errors=remount-ro 0 1" >> /etc/fstab
+  mount -a
+  mkdir -p /mnt/wfs/whisper
+fi
