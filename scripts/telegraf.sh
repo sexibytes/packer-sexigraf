a2e397bb305d6a8bd0fc62c99c28a86b57d87849
Hypervisor <raphael@schitz.net>
2020-05-03 13:08:56 +0200

    Update telegraf.sh
    #208

--- scripts/telegraf.sh
+++ scripts/telegraf.sh
@@ -1,7 +1,7 @@
- echo "install telegraf"
+echo "install telegraf"
 wget -qO- https://repos.influxdata.com/influxdb.key | apt-key add -
-source /etc/lsb-release
-echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | tee /etc/apt/sources.list.d/influxdb.list
+. /etc/lsb-release
+echo "deb https://repos.influxdata.com/$(DISTRIB_ID,,) $(DISTRIB_CODENAME) stable" | tee /etc/apt/sources.list.d/influxdb.list
 #
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y telegraf
@@ -9,3 +9,4 @@ systemctl start telegraf
 #
 echo "install ipmitool"
 DEBIAN_FRONTEND=noninteractive apt-get install -y ipmitool
+update-rc.d openipmi disable
