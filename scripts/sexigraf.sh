8236445dfe7c23b102e7c3ff607bf71151c1b987
Raphaël SCHITZ <raphael@schitz.net>
2020-11-20 14:20:21 +0100

    Update sexigraf.sh

--- scripts/sexigraf.sh
+++ scripts/sexigraf.sh
@@ -1,7 +1,7 @@
 # retrieve and unzip source
 echo "Retrieving SexiGraf sources"
 #
-wget -q --no-proxy https://github.com/sexibytes/sexigraf/archive/dev6.zip -O /tmp/sexigraf-src.zip
+wget -q --no-proxy https://github.com/sexibytes/sexigraf/archive/develop.zip -O /tmp/sexigraf-src.zip
 unzip /tmp/sexigraf-src.zip -d /tmp/
 
 # Create empty folder for logs
