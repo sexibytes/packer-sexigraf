95c5a17f4a18890444bf20b9d1c6e5bcc8e57f95
Raphaël SCHITZ <raphael@schitz.net>
2020-12-18 18:17:45 +0100

    Update powershell.sh

--- scripts/powershell.sh
+++ scripts/powershell.sh
@@ -12,7 +12,10 @@ apt-get update
 # Install PowerShell
 apt-get install -y powershell
 
-#Install PowerCLI
+# Install PowerCLI
 # https://vdc-download.vmware.com/vmwb-repository/dcr-public/249b0685-9188-4214-bafe-db9132a8582d/2184c002-1c41-458a-8584-87a461c7da23/powercli1150-compat-matrix.html
 /usr/bin/pwsh -Command 'Install-Module -Name VMware.PowerCLI -Scope AllUsers -Force -Confirm:$false'
 /usr/bin/pwsh -Command 'Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -ParticipateInCEIP $false -ProxyPolicy noproxy -DisplayDeprecationWarnings $false -DefaultVIServerMode single -Scope AllUsers -Confirm:$false'
+
+# clone Graphite-PowerShell-Functions
+git clone https://github.com/MattHodge/Graphite-PowerShell-Functions.git /usr/local/share/powershell/Modules/Graphite-PowerShell-Functions
