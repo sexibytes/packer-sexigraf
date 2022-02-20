2e3ff681fcf0dd9fab4017eefdb3248b8f67f8aa
Raphaël SCHITZ <raphael@schitz.net>
2022-02-20 23:16:45 +0100

    Update netdata.sh

--- scripts/netdata.sh
+++ scripts/netdata.sh
@@ -5,11 +5,11 @@ wget https://my-netdata.io/kickstart.sh -O /root/kickstart.sh
 bash /root/kickstart.sh all --dont-wait --non-interactive --no-updates --stable-channel
 # bash <(curl -Ss https://my-netdata.io/kickstart.sh) all --dont-wait --non-interactive --no-updates --stable-channel
 # https://github.com/netdata/netdata/issues/1735
-sed -i -e "s/# x-frame-options response header =/x-frame-options response header = sameorigin/g" /etc/netdata/netdata.conf
-sed -i -e "s/# hostname = sexigraf/# hostname = sexigraf\n          bind socket to IP = 127.0.0.1/g" /etc/netdata/netdata.conf
+# sed -i -e "s/# x-frame-options response header =/x-frame-options response header = sameorigin/g" /etc/netdata/netdata.conf
+# sed -i -e "s/# hostname = sexigraf/# hostname = sexigraf\n          bind socket to IP = 127.0.0.1/g" /etc/netdata/netdata.conf
 # 
 # You can opt out from anonymous statistics via the --disable-telemetry option, or by creating an empty file /etc/netdata/.opt-out-from-anonymous-statistics
 touch /etc/netdata/.opt-out-from-anonymous-statistics
 #
-systemctl restart netdata
+# systemctl restart netdata
 rm -f /root/kickstart.sh
