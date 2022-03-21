b9f92604134ef4b5480fece3ac7b659378f69d88
Raphaël SCHITZ <raphael@schitz.net>
2022-03-21 11:47:42 +0100

    Update powershell.sh

--- scripts/powershell.sh
+++ scripts/powershell.sh
@@ -1,16 +1,6 @@
-# https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux?view=powershell-7
-# Download the Microsoft repository GPG keys
-wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
-
-# Register the Microsoft repository GPG keys
-dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-
-# Update the list of products
-apt-get update
-
-# Install PowerShell
-apt-get install -y powershell
+# https://docs.microsoft.com/en-us/powershell/scripting/install/install-ubuntu?view=powershell-7.2
+wget https://github.com/PowerShell/PowerShell/releases/download/v7.2.1/powershell-lts_7.2.1-1.deb_amd64.deb -O /tmp/powershell.deb
+DEBIAN_FRONTEND=noninteractive dpkg -i /tmp/powershell.deb
 
 # Install PowerCLI
 # https://vdc-download.vmware.com/vmwb-repository/dcr-public/249b0685-9188-4214-bafe-db9132a8582d/2184c002-1c41-458a-8584-87a461c7da23/powercli1150-compat-matrix.html
