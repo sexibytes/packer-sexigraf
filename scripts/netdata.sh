2442e8e549395ca19564139ae797f2f8c03d611c
Raphaël SCHITZ <raphael@schitz.net>
2022-07-28 16:42:56 +0200

    Update netdata.sh

--- scripts/netdata.sh
+++ scripts/netdata.sh
@@ -2,11 +2,13 @@
 # DEBIAN_FRONTEND=noninteractive  apt-get -y install autoconf-archive autogen cmake libjudy-dev liblz4-dev libmnl-dev libuv1-dev python3-pymongo
 # 
 wget https://my-netdata.io/kickstart.sh -O /root/kickstart.sh
-sh /root/kickstart.sh --non-interactive --no-updates --stable-channel --disable-telemetry
+sh /root/kickstart.sh --non-interactive --no-updates --stable-channel --disable-telemetry --disable-cloud
 # bash <(curl -Ss https://my-netdata.io/kickstart.sh) all --dont-wait --non-interactive --no-updates --stable-channel
 # https://github.com/netdata/netdata/issues/1735
 # sed -i -e "s/# x-frame-options response header =/x-frame-options response header = sameorigin/g" /etc/netdata/netdata.conf
 # sed -i -e "s/# hostname = sexigraf/# hostname = sexigraf\n          bind socket to IP = 127.0.0.1/g" /etc/netdata/netdata.conf
+echo "    bind socket to IP = 127.0.0.1" >> /etc/netdata/netdata.conf
+
 # 
 # You can opt out from anonymous statistics via the --disable-telemetry option, or by creating an empty file /etc/netdata/.opt-out-from-anonymous-statistics
 touch /etc/netdata/.opt-out-from-anonymous-statistics
