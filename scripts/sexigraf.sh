f04ab1721a934274fab9547ec6727a801972fca4
Raphaël SCHITZ <raphael@schitz.net>
2023-01-03 00:03:21 +0100

    Update sexigraf.sh
    #278

--- scripts/sexigraf.sh
+++ scripts/sexigraf.sh
@@ -16,52 +16,6 @@ apt-get update -y
 # DEBIAN_FRONTEND=noninteractive apt-get -y install libhtml-template-perl libnumber-bytes-human-perl xml-twig-tools libsnmp30 cpanminus genisoimage collectd lib32z1 lib32ncurses5 build-essential uuid uuid-dev libssl-dev perl-doc libxml-libxml-perl libcrypt-ssleay-perl libsoap-lite-perl libmodule-build-perl libxml2 cpanminus sysstat snmp memcached net-tools
 DEBIAN_FRONTEND=noninteractive apt-get -y install xml-twig-tools genisoimage collectd-core lib32z1 build-essential uuid uuid-dev libssl-dev libxml2 sysstat snmp memcached net-tools snmp-mibs-downloader
 
-# https://code.vmware.com/docs/6530/vsphere-sdk-for-perl-installation-guide/doc/GUID-16A5A35D-1E05-4DD4-8E02-BEA6BF24A77B.html
-# https://vdc-repo.vmware.com/vmwb-repository/dcr-public/f280c443-0cda-4fed-8e15-7dc07e2b7037/66ce9472-ffd3-4e80-83b4-1bcfeec2099e/doc/GUID-8B0E6E94-A215-4904-935D-1B164C3941A8.html#GUID-8B0E6E94-A215-4904-935D-1B164C3941A8
-# https://vdc-download.vmware.com/vmwb-repository/dcr-public/ae41a1d3-b1ac-4f7c-a1d4-4774ddd05e99/2d5ae4b7-d040-4de1-824d-eb339b09cf6e/vsphere-perl-sdk-67-release-notes.html#supported
-# https://communities.vmware.com/message/2298661#2298661
-
-# /bin/cp -rf /tmp/sexigraf-develop/root/* /root/
-# tar -zxf /root/VMware-vSphere-Perl-SDK-7.0.0-17698549.x86_64.tar.gz -C /root/
-
-# cpanm ExtUtils::MakeMaker
-# cpanm Net::FTP
-# cpanm Module::Build
-# #
-# cpanm Time::Piece
-# cpanm Archive::Zip
-# cpanm ExtUtils::Installed
-# cpanm Path::Class
-# cpanm Try::Tiny
-# cpanm Crypt::SSLeay
-# cpanm version
-# cpanm Data::Dumper
-# cpanm HTML::Parser
-# cpanm UUID
-# cpanm XML::SAX
-# cpanm XML::NamespaceSupport
-# cpanm XML::LibXML::Common
-# cpanm XML::LibXML
-# cpanm LWP
-# cpanm LWP::Protocol::https
-# cpanm Socket6
-# cpanm Text::Template
-# cpanm IO::Socket::INET6
-# cpanm Net::INET6Glue
-# cpanm Net::HTTP
-# #
-# cpanm Net::Graphite
-# cpanm Log::Log4perl
-# cpanm JSON
-# cpanm Sys::SigAction
-
-# yes | PAGER=cat /root/vmware-vsphere-cli-distrib/vmware-install.pl default
-
-# sed -i 's/# $ENV{PERL_NET_HTTPS_SSL_SOCKET_CLASS}/$ENV{PERL_NET_HTTPS_SSL_SOCKET_CLASS}/g' /usr/share/perl/*/VMware/VICommon.pm
-
-# export PERL_MM_USE_DEFAULT=1
-# cpan -f -i GAAS/libwww-perl-5.837.tar.gz
-
 /bin/cp -rf /tmp/sexigraf-develop/etc/* /etc/
 /bin/cp -rf /tmp/sexigraf-develop/usr/* /usr/
 /bin/cp -rf /tmp/sexigraf-develop/var/* /var/
@@ -146,9 +100,6 @@ a2enmod socache_shmcb
 a2enmod rewrite
 
 chmod a+x /opt/sexigraf/PullGuestInfo.sh
-# chmod a+x /root/ViPullStatistics.pl
-# chmod a+x /root/VsanPullStatistics.pl
-# chmod a+x /root/getInventory.pl
 chmod a+x /opt/sexigraf/*.ps1
 
 echo "Intialise empty PS credentials store"
@@ -159,9 +110,6 @@ mkdir -p /var/log/sexigraf
 mkdir -p /var/log/apache2/graphite
 
 # Configure crontab for vmtools infos
-echo "\n@reboot         root    /bin/bash /opt/sexigraf/PullGuestInfo.sh" >> /etc/crontab
-
-# echo "Removing unused files"
-# rm -f /root/VMware-vSphere-Perl-SDK-*.tar.gz
-# rm -rf /root/vmware-vsphere-cli-distrib/
-# echo "SexiDone"
+echo "\n@reboot         root    /usr/bin/pwsh -NonInteractive -NoProfile -f /opt/sexigraf/PullGuestInfo.ps1 >/dev/null 2>&1" >> /etc/crontab
+#
+echo "SexiDone"
