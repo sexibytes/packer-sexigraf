01aa3cee003dc3da45ac9d0016e908dcffd33f8b
Hypervisor <raphael@schitz.net>
2024-02-05 18:54:22 +0100

    Update telegraf.sh

--- scripts/telegraf.sh
+++ scripts/telegraf.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo "install telegraf"
-wget -qO- https://repos.influxdata.com/influxdb.key | apt-key add -
-echo "deb https://repos.influxdata.com/ubuntu focal stable" > /etc/apt/sources.list.d/influxdb.list
+#
+echo '943666881a1b8d9b849b74caebf02d3465d6beb716510d86a39f6c8e8dac7515 influxdata-archive.key' | sha256sum -c && cat influxdata-archive.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/influxdata-archive.gpg > /dev/null
+#
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive.gpg] https://repos.influxdata.com/debian stable main' | tee /etc/apt/sources.list.d/influxdata.list
 #
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y telegraf
-systemctl start telegraf
+apt-get install telegraf -y
 #
-echo "install ipmitool"
+# echo "install ipmitool"
 # DEBIAN_FRONTEND=noninteractive apt-get install -y ipmitool
 # update-rc.d openipmi disable
