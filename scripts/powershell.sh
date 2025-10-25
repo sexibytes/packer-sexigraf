fd6f33538d545df1cb0f03d11766aff52d32dcf3
Raphaël SCHITZ <raphael@schitz.net>
2025-10-25 10:48:43 +0200

    Update powershell.sh
    https://github.com/sexibytes/sexigraf/issues/430

--- scripts/powershell.sh
+++ scripts/powershell.sh
@@ -4,7 +4,8 @@ DEBIAN_FRONTEND=noninteractive dpkg -i /tmp/powershell.deb
 
 # Install PowerCLI
 # https://vdc-download.vmware.com/vmwb-repository/dcr-public/249b0685-9188-4214-bafe-db9132a8582d/2184c002-1c41-458a-8584-87a461c7da23/powercli1150-compat-matrix.html
-/usr/bin/pwsh -Command 'Install-Module -Name VMware.PowerCLI -Scope AllUsers -Force -Confirm:$false'
+# /usr/bin/pwsh -Command 'Install-Module -Name VMware.PowerCLI -Scope AllUsers -Force -Confirm:$false'
+/usr/bin/pwsh -Command 'Install-Module -Name VCF.PowerCLI -Scope AllUsers -Force -Confirm:$false'
 /usr/bin/pwsh -Command 'Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -ParticipateInCEIP $false -ProxyPolicy noproxy -DisplayDeprecationWarnings $false -DefaultVIServerMode single -Scope AllUsers -Confirm:$false'
 
 # clone Graphite-PowerShell-Functions
