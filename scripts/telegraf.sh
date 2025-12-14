019fa89ecfac503846587b83cb98ff039c644fd8
Raphaël SCHITZ <raphael@schitz.net>
2025-12-15 00:25:16 +0100

    Update telegraf.sh

--- scripts/telegraf.sh
+++ scripts/telegraf.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 echo "install telegraf"
 #
-curl -s https://repos.influxdata.com/influxdata-archive.key > /tmp/influxdata-archive.key
-#
-echo '943666881a1b8d9b849b74caebf02d3465d6beb716510d86a39f6c8e8dac7515 /tmp/influxdata-archive.key' | sha256sum
-cat /tmp/influxdata-archive.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/influxdata-archive.gpg > /dev/null
-echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive.gpg] https://repos.influxdata.com/debian stable main' | tee /etc/apt/sources.list.d/influxdata.list
+curl -s https://repos.influxdata.com/influxdata-archive_compat.key > /tmp/influxdata-archive.key
+echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c /tmp/influxdata-archive.key' | sha256sum -c
+cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
 #
 apt-get update
 apt-get install telegraf -y
